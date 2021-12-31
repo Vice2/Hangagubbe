ec742624110209bae1aff4f93d554221a8cf41d5
Vice2 <90139249+Vice2@users.noreply.github.com>
2021-12-31 11:07:44 +0100

    Initial Commit

---
+++ Hangagubbe/HangagubbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangagubbeApp.swift
+//  Hangagubbe
+//
+//  Created by Bashar Al Bagdadi on 2021-12-31.
+//
+
+import SwiftUI
+
+@main
+struct HangagubbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
